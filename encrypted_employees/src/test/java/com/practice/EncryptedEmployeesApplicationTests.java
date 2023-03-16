b93c38e341f3a9284505881f26440e46afb38706
Gloria Sevilla <gloriasevilla76@yahoo.es>
2023-03-16 17:13:40 -0600

    I created the new project called "encrypted employees".

---
+++ encrypted_employees/src/test/java/com/practice/EncryptedEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptedEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
